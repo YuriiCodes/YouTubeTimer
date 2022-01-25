6c8d22d629e003ca26d907dfdfc81659a34c6817
Yurii <yurii@Yuriis-MacBook-Air.local>
2022-01-25 19:17:33 +0200

    initial commit

--- Chapter4/event.js
+++
@@ -1,8 +0,0 @@
-﻿chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    // Show page action if URL contains yahoo
-    if (changeInfo.status == "complete"){
-        if (tab.url.indexOf('yahoo') != -1){
-            chrome.pageAction.show(tabId);
-        }
-    }
-});
